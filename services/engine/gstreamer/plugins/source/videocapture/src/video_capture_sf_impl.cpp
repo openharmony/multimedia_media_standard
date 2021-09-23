bf5fc9b60097328831d2355553cd7fbcc7ef2b49
bird_j <jianghongpeng@huawei.com>
2021-09-23 11:03:13 +0000

    增加新的分辨率适配 Signed-off-by: bird_j <jianghongpeng@huawei.com>

--- services/engine/gstreamer/plugins/source/videocapture/src/video_capture_sf_impl.cpp
+++ services/engine/gstreamer/plugins/source/videocapture/src/video_capture_sf_impl.cpp
@@ -33,6 +33,7 @@ const std::map<uint32_t, uint32_t> VIDEO_RESOLUTION_MAP = {
     { 1920, 1080 },
     { 1280, 720 },
     { 720, 480 },
+    { 640, 360 },
 };
 VideoCaptureSfImpl::VideoCaptureSfImpl()
     : videoWidth_(DEFAULT_VIDEO_WIDTH),
