cdea46f3c25502af3441981162e82e37c9785125
bird_j <jianghongpeng@huawei.com>
2021-09-29 12:08:16 +0000

    同步LTS代码 Signed-off-by: bird_j <jianghongpeng@huawei.com>

--- services/engine/gstreamer/plugins/source/videocapture/src/video_capture_sf_impl.cpp
+++ services/engine/gstreamer/plugins/source/videocapture/src/video_capture_sf_impl.cpp
@@ -33,6 +33,7 @@ const std::map<uint32_t, uint32_t> VIDEO_RESOLUTION_MAP = {
     { 1920, 1080 },
     { 1280, 720 },
     { 720, 480 },
+    { 640, 360 },
 };
 
 VideoCaptureSfImpl::VideoCaptureSfImpl()
@@ -128,6 +129,7 @@ void VideoCaptureSfImpl::SetEndOfStream(bool endOfStream)
     bufferAvailableCondition_.notify_all();
     MEDIA_LOGI("notify end of stream: %{public}d", isEos);
 }
+
 int32_t VideoCaptureSfImpl::SetVideoWidth(uint32_t width)
 {
     videoWidth_ = width;
@@ -216,7 +218,7 @@ int32_t VideoCaptureSfImpl::GetSufferExtraData()
     surfaceRet = surfaceBuffer_->ExtraGet("isKeyFrame", isCodecFrame_);
     CHECK_AND_RETURN_RET_LOG(surfaceRet == SURFACE_ERROR_OK, MSERR_INVALID_OPERATION, "get isKeyFrame fail");
 
-    MEDIA_LOGI("surfaceBuffer extraData dataSize_: %{public}d, pts: (%{public}" PRId64 ")", dataSize_, pts_);
+    MEDIA_LOGI("surfaceBuffer extraData dataSize_: %{public}d, pts: %{public}" PRId64 "", dataSize_, pts_);
     MEDIA_LOGI("is this surfaceBuffer keyFrame ? : %{public}d", isCodecFrame_);
     return MSERR_OK;
 }
