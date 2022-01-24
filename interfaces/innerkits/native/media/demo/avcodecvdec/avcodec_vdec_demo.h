7ef8f6296907479ccaaeb004734f25aa3e3dd6e0
abc12133 <jinguoen@huawei.com>
2022-01-24 19:30:36 +0800

    add vdec deni
    Signed-off-by: abc12133 <jinguoen@huawei.com>

---
+++ interfaces/innerkits/native/media/demo/avcodecvdec/avcodec_vdec_demo.h
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef AVCODEC_VDEC_DEMO_H
+#define AVCODEC_VDEC_DEMO_H
+
+#include <atomic>
+#include <fstream>
+#include <thread>
+#include <queue>
+#include <string>
+#include "avcodec_video_decoder.h"
+#include "nocopyable.h"
+
+namespace OHOS {
+namespace Media {
+class VDecSignal {
+public:
+    std::mutex inMutex_;
+    std::mutex outMutex_;
+    std::condition_variable inCond_;
+    std::condition_variable outCond_;
+    std::queue<uint32_t> inQueue_;
+    std::queue<uint32_t> outQueue_;
+};
+
+class VDecDemoCallback : public AVCodecCallback {
+public:
+    explicit VDecDemoCallback(std::shared_ptr<VDecSignal> signal);
+    virtual ~VDecDemoCallback() = default;
+    DISALLOW_COPY_AND_MOVE(VDecDemoCallback);
+
+    void OnError(AVCodecErrorType errorType, int32_t errorCode) override;
+    void OnOutputFormatChanged(const Format &format) override;
+    void OnInputBufferAvailable(uint32_t index) override;
+    void OnOutputBufferAvailable(uint32_t index, AVCodecBufferInfo info, AVCodecBufferFlag flag) override;
+
+private:
+    std::shared_ptr<VDecSignal> signal_;
+};
+
+class VDecDemo {
+public:
+    VDecDemo() = default;
+    virtual ~VDecDemo() = default;
+    DISALLOW_COPY_AND_MOVE(VDecDemo);
+    void RunCase();
+
+private:
+    int32_t CreateVdec();
+    int32_t Configure(const Format &format);
+    int32_t Prepare();
+    int32_t Start();
+    int32_t Stop();
+    int32_t Flush();
+    int32_t Reset();
+    int32_t Release();
+    int32_t SetSurface();
+    void InputFunc();
+    void OutputFunc();
+
+    std::atomic<bool> isRunning_ = false;
+    std::unique_ptr<std::ifstream> testFile_;
+    std::unique_ptr<std::thread> inputLoop_;
+    std::unique_ptr<std::thread> outputLoop_;
+    std::shared_ptr<VideoDecoder> vdec_;
+    std::shared_ptr<VDecSignal> signal_;
+    std::shared_ptr<VDecDemoCallback> cb_;
+    bool isFirstFrame_ = true;
+    int64_t timeStamp_ = 0;
+    uint32_t frameCount_ = 0;
+};
+} // Media
+} // OHOS
+#endif // AVCODEC_VDEC_DEMO_H
