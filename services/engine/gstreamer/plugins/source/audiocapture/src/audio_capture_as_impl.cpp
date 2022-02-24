2f1ae31fec8ee52a4ca1887b36a831dc977b00b0
bird_j <jianghongpeng@huawei.com>
2022-02-24 14:59:24 +0800

    bugfix
    Signed-off-by: bird_j <jianghongpeng@huawei.com>

--- services/engine/gstreamer/plugins/source/audiocapture/src/audio_capture_as_impl.cpp
+++ services/engine/gstreamer/plugins/source/audiocapture/src/audio_capture_as_impl.cpp
@@ -182,10 +182,18 @@ int32_t AudioCaptureAsImpl::StopAudioCapture()
     return MSERR_OK;
 }
 
+uint64_t AudioCaptureAsImpl::GetCurrentTime()
+{
+    struct timespec timestamp = {0, 0};
+    clock_gettime(CLOCK_MONOTONIC, &timestamp);
+    uint64_t time = (uint64_t)timestamp.tv_sec * SEC_TO_NS + (uint64_t)timestamp.tv_nsec;
+    return time;
+}
+
 int32_t AudioCaptureAsImpl::PauseAudioCapture()
 {
     MEDIA_LOGD("PauseAudioCapture");
-    pausedTime_ = timestamp_;
+    pausedTime_ = GetCurrentTime();
 
     CHECK_AND_RETURN_RET(audioCapturer_ != nullptr, MSERR_INVALID_OPERATION);
     if (audioCapturer_->GetStatus() == AudioStandard::CapturerState::CAPTURER_RUNNING) {
@@ -199,7 +207,7 @@ int32_t AudioCaptureAsImpl::PauseAudioCapture()
 int32_t AudioCaptureAsImpl::ResumeAudioCapture()
 {
     MEDIA_LOGD("ResumeAudioCapture");
-    resumeTime_ = timestamp_;
+    resumeTime_ = GetCurrentTime();
 
     if (resumeTime_ < pausedTime_) {
         MEDIA_LOGW("get wrong timestamp from audio services!");
