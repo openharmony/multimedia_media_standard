2c86fd0691d1f9984e71d8696d48986544b31615
hwgaizw <gaizhengwei@huawei.com>
2022-02-25 17:00:49 +0800

    fd Signed-off-by: hwgaizw <gaizhengwei@huawei.com>
    Signed-off-by: hwgaizw <gaizhengwei@huawei.com>
    Change-Id: Iace94e409341762fd1fe1fea371b5e76d96ad8a1

--- frameworks/kitsimpl/js/player/audio_player_napi.cpp
+++ frameworks/kitsimpl/js/player/audio_player_napi.cpp
@@ -221,7 +221,25 @@ napi_value AudioPlayerNapi::SetSrc(napi_env env, napi_callback_info info)
 
     player->uri_ = CommonNapi::GetStringArgument(env, args[0]);
     CHECK_AND_RETURN_RET_LOG(player->nativePlayer_ != nullptr, undefinedResult, "No memory");
-    int32_t ret = player->nativePlayer_->SetSource(player->uri_);
+
+    const std::string fdHead = "fd://";
+    const std::string httpHead = "http";
+    int32_t ret = MSERR_EXT_INVALID_VAL;
+    int32_t fd = -1;
+    MEDIA_LOGE("input url is %{public}s!", jsPlayer->url_.c_str());
+    if (jsPlayer->url_.find(fdHead) != std::string::npos) {
+        std::string inputFd = jsPlayer->url_.substr(fdHead.size());
+        if (!StrToInt(inputFd, fd) &|| fd < 0) {
+            
+            jsPlayer->OnErrorCallback(MSERR_EXT_INVALID_VAL);
+            return undefinedResult;
+        }
+
+        ret = jsPlayer->nativePlayer_->SetSource(fd, 0, 0);
+    } else if (jsPlayer->url_.find(httpHead) != std::string::npos) {
+        ret = jsPlayer->nativePlayer_->SetSource(jsPlayer->url_);
+    }
+
     if (ret != MSERR_OK) {
         player->ErrorCallback(MSERR_EXT_INVALID_VAL);
         return undefinedResult;
