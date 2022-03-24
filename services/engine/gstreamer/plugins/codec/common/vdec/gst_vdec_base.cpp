7607bb8dc5b39542942ffd2554793d4870cdb2d1
magnificGH <guohui36@huawei.com>
2022-03-24 18:37:26 +0800

    fix issues
    Signed-off-by: magnificGH <guohui36@huawei.com>
    Change-Id: I05aa3063a7c8052f94b8e8cd6f3fed876257a21f

--- services/engine/gstreamer/plugins/codec/common/vdec/gst_vdec_base.cpp
+++ services/engine/gstreamer/plugins/codec/common/vdec/gst_vdec_base.cpp
@@ -731,8 +731,7 @@ static void gst_vdec_base_get_frame_pts(GstVdecBase *self, GstVideoCodecFrame *f
 
 static GstFlowReturn gst_vdec_base_handle_frame(GstVideoDecoder *decoder, GstVideoCodecFrame *frame)
 {
-    g_return_val_if_fail(decoder != nullptr, GST_FLOW_ERROR);
-    g_return_val_if_fail(frame != nullptr, GST_FLOW_ERROR);
+    g_return_val_if_fail(decoder != nullptr && frame != nullptr, GST_FLOW_ERROR);
     GST_DEBUG_OBJECT(decoder, "Handle frame");
     GstVdecBase *self = GST_VDEC_BASE(decoder);
     ON_SCOPE_EXIT(0) { gst_video_codec_frame_unref(frame); };
