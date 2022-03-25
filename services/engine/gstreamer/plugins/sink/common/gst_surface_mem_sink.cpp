ef8e769426a98b403abeff60e78338938b8733a1
hwgaizw <gaizhengwei@huawei.com>
2022-03-25 11:42:16 +0800

    audio js api Signed-off-by: hwgaizw <gaizhengwei@huawei.com>
    Signed-off-by: hwgaizw <gaizhengwei@huawei.com>
    Change-Id: If58167a162c6f20d27086277d1f3c665b93cf10e

--- services/engine/gstreamer/plugins/sink/common/gst_surface_mem_sink.cpp
+++ services/engine/gstreamer/plugins/sink/common/gst_surface_mem_sink.cpp
@@ -190,11 +190,11 @@ static GstFlowReturn gst_surface_mem_sink_do_app_render(GstMemSink *memsink, Gst
     GST_OBJECT_LOCK(surface_sink);
 
     if (surface_sink->firstRenderFrame && isPreroll) {
-        surface_sink->firstRenderFrame = FALSE;
-        GST_DEBUG_OBJECT(surface_sink, "fisrt render frame");
+        GST_WARNING_OBJECT(surface_sink, "fisrt render frame");
         GST_OBJECT_UNLOCK(surface_sink);
         return GST_FLOW_OK;
     }
+    surface_sink->firstRenderFrame = FALSE;
 
     for (guint i = 0; i < gst_buffer_n_memory(buffer); i++) {
         GstMemory *memory = gst_buffer_peek_memory(buffer, i);
