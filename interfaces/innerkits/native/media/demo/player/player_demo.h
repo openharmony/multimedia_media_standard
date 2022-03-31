bc396b0c9e981fc57d1e8e8d5e02f3b017d4db05
huang-xl <huangxiaolong1@huawei.com>
2022-03-31 14:46:47 +0800

    clean wm Signed-off-by: huang-xl<huangxiaolong1@huawei.com>
    Signed-off-by: huang-xl <huangxiaolong1@huawei.com>
    Change-Id: Iefb9fc9c2cd8725128727f771323ec392c6386d7

--- interfaces/innerkits/native/media/demo/player/player_demo.h
+++ interfaces/innerkits/native/media/demo/player/player_demo.h
@@ -18,12 +18,11 @@
 
 #include "securec.h"
 #include "player.h"
+#include "window.h"
 #include "surface.h"
 #include "display_type.h"
-#include "window_manager.h"
 #include "nocopyable.h"
 #include "media_data_source_demo.h"
-#include "foundation/windowmanager/interfaces/innerkits/wm/window.h"
 
 namespace MediaDemo {
     const int32_t POSITION_UPDATE_INTERVAL = 100;
@@ -69,7 +68,6 @@ private:
     int32_t ChangeSpeedToMode(const double &rate, PlaybackRateMode &mode) const;
     sptr<Surface> GetSubWindowSurface();
     void RegisterTable();
-    sptr<Window> mwindow_ = nullptr;
     sptr<Rosen::Window> previewWindow_ = nullptr;
     std::map<std::string, std::function<int32_t()>> playerTable_;
     std::shared_ptr<Player> player_ = nullptr;
