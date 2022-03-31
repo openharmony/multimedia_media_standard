87ed91bb14aaba38c9edd15f957b48ca28d0b974
magnificGH <guohui36@huawei.com>
2022-03-31 11:22:41 +0800

    add avmuxer napi\demo\impl
    Signed-off-by: magnificGH <guohui36@huawei.com>

---
+++ interfaces/innerkits/native/media/demo/avmuxer/avmuxer_demo.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef AVMUXER_DEMO_H
+#define AVMUXER_DEMO_H
+
+#include "avmuxer.h"
+
+namespace OHOS {
+namespace Media {
+class AVMuxerDemo : public NoCopyable {
+public:
+    AVMuxerDemo() = default;
+    ~AVMuxerDemo() = default;
+    void RunCase();
+private:
+    bool PushBuffer(std::shared_ptr<std::ifstream> File, const int32_t *FrameArray,
+        int32_t i, int32_t TrakcId_, int64_t stamp);
+    void WriteTrackSample();
+    bool AddTrackVideo(std::string& videoType);
+    bool AddTrackAudio(std::string& audioType);
+    void SetParameter(std::string type);
+    void DoNext();
+    std::shared_ptr<AVMuxer> avmuxer_;
+    int32_t videoTrakcId_ = 0;
+    int32_t audioTrackId_ = 0;
+    int32_t videoTimeDuration_ = 0;
+    int32_t audioTimeDuration_ = 0;
+    uint32_t videoFrameNum_ = 0;
+    uint32_t audioFrameNum_ = 0;
+    const int32_t *videoFrameArray_ = nullptr;
+    const int32_t *audioFrameArray_ = nullptr;
+    std::shared_ptr<std::ifstream> videoFile_ = nullptr;
+    std::shared_ptr<std::ifstream> audioFile_ = nullptr;
+    std::string videoType_ = std::string("");
+    std::string audioType_ = std::string("");
+};
+}  // namespace Media
+}  // namespace OHOS
+#endif  // AVMUXER_DEMO_H
