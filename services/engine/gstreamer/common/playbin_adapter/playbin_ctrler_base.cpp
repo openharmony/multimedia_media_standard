f11c67997e302150c751ca5b020c50671a5916dd
z00502384 <zhangyue117@huawei.com>
2022-06-29 09:56:50 +0800

    播放内存泄露
    Signed-off-by: z00502384 <zhangyue117@huawei.com>
    Change-Id: I1f164f3571ebc13445b4346dcc58587a5f36e667

--- services/engine/gstreamer/common/playbin_adapter/playbin_ctrler_base.cpp
+++ services/engine/gstreamer/common/playbin_adapter/playbin_ctrler_base.cpp
@@ -659,6 +659,7 @@ void PlayBinCtrlerBase::SetupCustomElement()
         GstElement *audioFilter = gst_element_factory_make("scaletempo", "scaletempo");
         if (audioFilter != nullptr) {
             g_object_set(playbin_, "audio-filter", audioFilter, nullptr);
+            gst_object_unref(audioFilter);
         } else {
             MEDIA_LOGD("can not create scaletempo, the audio playback speed can not be adjusted");
         }
