7064938b497ce94497eb04065cbbcd4f30747aae
xxb-wzy <xuxiaobo5@huawei.com>
2022-06-30 12:32:04 +0800

    fix codec mem flow
    Signed-off-by: xxb-wzy <xuxiaobo5@huawei.com>
    Change-Id: If2b7de4d4c9e1179a4382376ff9946de0172b0d8

--- services/services/avcodec/server/avcodec_server.h
+++ services/services/avcodec/server/avcodec_server.h
@@ -68,6 +68,7 @@ public:
 
 private:
     int32_t Init();
+    void TaskProcessor();
     const std::string &GetStatusDescription(OHOS::Media::AVCodecServer::AVCodecStatus status);
 
     AVCodecStatus status_ = AVCODEC_UNINITIALIZED;
