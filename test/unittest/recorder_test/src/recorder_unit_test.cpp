812ff7a1bc1dded3f8b038dcd191d3299ffa5009
NOBUGGERS <ruanmeng@huawei.com>
2022-07-14 10:27:46 +0800

    TDD test update recorder/player Signed-off-by: NOBUGGERS <ruanmeng@huawei.com>
    Signed-off-by: NOBUGGERS <ruanmeng@huawei.com>

--- test/unittest/recorder_test/src/recorder_unit_test.cpp
+++ test/unittest/recorder_test/src/recorder_unit_test.cpp
@@ -69,28 +69,6 @@ HWTEST_F(RecorderUnitTest, recorder_video_yuv_mpeg4, TestSize.Level0)
     close(g_videoRecorderConfig.outputFd);
 }
 
-/**
- * @tc.name: recorder_video_es
- * @tc.desc: recorde video with es
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(RecorderUnitTest, recorder_video_es, TestSize.Level0)
-{
-    g_videoRecorderConfig.vSource = VIDEO_SOURCE_SURFACE_ES;
-    g_videoRecorderConfig.outputFd = open("/data/test/recorder_video_es.mp4", O_RDWR);
-    ASSERT_TRUE(g_videoRecorderConfig.outputFd >= 0);
-
-    EXPECT_EQ(MSERR_OK, recorder_->SetFormat(PURE_VIDEO, g_videoRecorderConfig));
-    EXPECT_EQ(MSERR_OK, recorder_->Prepare());
-    EXPECT_EQ(MSERR_OK, recorder_->RequesetBuffer(PURE_VIDEO, g_videoRecorderConfig));
-    EXPECT_EQ(MSERR_OK, recorder_->Start());
-    sleep(RECORDER_TIME);
-    EXPECT_EQ(MSERR_OK, recorder_->Stop(false));
-    recorder_->StopBuffer(PURE_VIDEO);
-    EXPECT_EQ(MSERR_OK, recorder_->Release());
-    close(g_videoRecorderConfig.outputFd);
-}
 
 /**
  * @tc.name: recorder_audio_es
@@ -193,5 +171,25 @@ HWTEST_F(RecorderUnitTest, recorder_video_SetOrientationHint_001, TestSize.Level
     recorder_->StopBuffer(PURE_VIDEO);
     EXPECT_EQ(MSERR_OK, recorder_->Reset());
     EXPECT_EQ(MSERR_OK, recorder_->Release());
+    close(g_videoRecorderConfig.outputFd);
+}
+
+/**
+ * @tc.name: recorder_video_SetMaxFileSize_001
+ * @tc.desc: recorde video ,SetMaxFileSize
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(RecorderUnitTest, recorder_video_SetMaxFileSize_001, TestSize.Level0)
+{
+    g_videoRecorderConfig.vSource = VIDEO_SOURCE_SURFACE_YUV;
+    g_videoRecorderConfig.videoFormat = MPEG4;
+    g_videoRecorderConfig.outputFd = open("/data/test/recorder_video_SetMaxFileSize_001.mp4", O_RDWR);
+    ASSERT_TRUE(g_videoRecorderConfig.outputFd >= 0);
+    EXPECT_EQ(MSERR_OK, recorder_->SetFormat(PURE_VIDEO, g_videoRecorderConfig));
+    recorder_->SetCaptureRate(0, 30);
+    EXPECT_EQ(MSERR_OK, recorder_->SetMaxFileSize(5000));
+    EXPECT_EQ(MSERR_OK, recorder_->SetNextOutputFile(g_videoRecorderConfig.outputFd));
+    recorder_->SetFileSplitDuration(FileSplitType::FILE_SPLIT_POST, -1, 1000);
     close(g_videoRecorderConfig.outputFd);
 }
