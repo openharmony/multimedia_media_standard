5c7ddce8b9ad27b52f8bac62008aecbe8e7ec1d2
NOBUGGERS <ruanmeng@huawei.com>
2022-07-14 10:30:11 +0800

    TDD test update recorder/player Signed-off-by: NOBUGGERS <ruanmeng@huawei.com>
    Signed-off-by: NOBUGGERS <ruanmeng@huawei.com>

--- test/unittest/player_test/src/data_source/media_data_source_test.h
+++ test/unittest/player_test/src/data_source/media_data_source_test.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Copyright (C) 2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,8 +13,8 @@
  * limitations under the License.
  */
 
-#ifndef MEDIA_DATA_SOURCE_DEMO_H
-#define MEDIA_DATA_SOURCE_DEMO_H
+#ifndef MEDIA_DATA_SOURCE_TEST_H
+#define MEDIA_DATA_SOURCE_TEST_H
 
 #include <string>
 #include "media_data_source.h"
@@ -29,4 +29,4 @@ public:
 };
 } // namespace Media
 } // namespace OHOS
-#endif // MEDIA_DATA_SOURCE_DEMO_H_
+#endif // MEDIA_DATA_SOURCE_TEST_H_
