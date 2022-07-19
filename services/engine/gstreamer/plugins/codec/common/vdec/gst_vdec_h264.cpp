73d5703eff0a80905388dffb06bd4e4519adbc2d
lin-jianwu <linjianwu@huawei.com>
2022-07-19 17:49:54 +0800

    fix review
    Signed-off-by: lin-jianwu <linjianwu@huawei.com>
    Change-Id: I59f1bba77b4764bd0f2a31aaf5b195e6fe20b29e

--- services/engine/gstreamer/plugins/codec/common/vdec/gst_vdec_h264.cpp
+++ services/engine/gstreamer/plugins/codec/common/vdec/gst_vdec_h264.cpp
@@ -85,9 +85,11 @@ static GstBuffer *handle_slice_buffer(GstVdecBase *self, GstBuffer *buffer, bool
     gboolean slice_flag = false;
     guint8 offset = 2;
     for (gsize i = 0; i < info.size - offset; i++) {
-        if (info.data[i] == 0x01 && (info.data[i + offset] & 0x80) == 0x80 && // 0x80 is nal flag of slice
-            (info.data[i + 1] == 0x65 || info.data[i + 1] == 0x41)) { // 0x65:I frame flag, 0x41:P、B frame flag
-            slice_flag = true;
+        if (info.data[i] == 0x01)
+            if ((info.data[i + 1] == 0x65 || info.data[i + 1] == 0x41)) // 0x65:I frame flag, 0x41:P、B frame flag
+                && (info.data[i + offset] & 0x80) == 0x80) { // 0x80 is nal flag of slice
+                slice_flag = true;
+            }
             break;
         }
     }
