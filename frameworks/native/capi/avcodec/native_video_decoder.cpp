467bde0a9f08999c9a7b22d69b510d3b5232516a
lin-jianwu <linjianwu@huawei.com>
2022-08-12 09:19:42 +0800

    fix name
    Signed-off-by: lin-jianwu <linjianwu@huawei.com>
    Change-Id: I4e0a92aa3c4a2ff9e1b5688fbe20fa685cb8b8da

--- frameworks/native/capi/avcodec/native_video_decoder.cpp
+++ frameworks/native/capi/avcodec/native_video_decoder.cpp
@@ -419,7 +419,8 @@ OH_AVErrCode OH_VideoDecoder_SetParameter(struct OH_AVCodec *codec, struct OH_AV
     return AV_ERR_OK;
 }
 
-OH_AVErrCode OH_VideoDecoder_SetCallback(struct OH_AVCodec *codec, struct OH_AVCodecAsyncCallback callback, void *userData)
+OH_AVErrCode OH_VideoDecoder_SetCallback(
+    struct OH_AVCodec *codec, struct OH_AVCodecAsyncCallback callback, void *userData)
 {
     CHECK_AND_RETURN_RET_LOG(codec != nullptr, AV_ERR_INVALID_VAL, "input codec is nullptr!");
     CHECK_AND_RETURN_RET_LOG(codec->magic_ == AVMagic::MEDIA_MAGIC_VIDEO_DECODER, AV_ERR_INVALID_VAL, "magic error!");
